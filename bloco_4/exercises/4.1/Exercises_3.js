8fbe0526907d574998d7ef76efcadc1d340d1ab3
Guilherme Almeida <almeidaguilherme204@gmail.com>
2020-08-27 18:12:26 -0300

    Exercício para retornar o maior de 3 números

---
+++ bloco_4/exercises/4.1/Exercises_3.js
@@ -0,0 +1,11 @@
+let valor1 = 45;
+let valor2 = 70;
+let valor3 = 48;
+
+if (valor1 > valor2 && valor1 > valor3) {
+    console.log(valor1);
+} else if ( valor2 > valor1 && valor2 > valor3) {
+    console.log(valor2);
+} else {
+    console.log(valor3);
+}
