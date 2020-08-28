c6ab0d9dccd589f23eb7ef9a5db1ce8e44192115
Guilherme Almeida <almeidaguilherme204@gmail.com>
2020-08-28 19:16:12 -0300

    Exercício para imprimir o menor número do array

--- bloco_4/Exercises_4.2/Exercise_6.js
+++ bloco_4/Exercises_4.2/Exercise_6.js
@@ -8,8 +8,11 @@ for(let i =0;i < numbers.length; i+=1){
         
     } 
 };
-if(numerosImpares > 0){
-    console.log(numerosImpares)
+if(numerosImpares > 0 && numerosImpares !== 1){
+    
+    console.log(`O array possui ${numerosImpares} números ímpares`)
+} else if (numerosImpares === 1){
+    console.log(`O array possui ${numerosImpares} número ímpar`)
 } else {
     console.log("Nenhum valor ímpar encontrado")
 };
