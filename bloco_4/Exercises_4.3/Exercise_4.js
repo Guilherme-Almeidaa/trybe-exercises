4e34adaf52321ac760965b4eedd1873bb7a96439
Guilherme Almeida <almeidaguilherme204@gmail.com>
2020-09-01 11:19:12 -0300

    Exercício para fazer uma pirâmide de *

---
+++ bloco_4/Exercises_4.3/Exercise_4.js
@@ -0,0 +1,21 @@
+let n = 7;
+let linha ="";
+let number = (n+1)/2;
+let esquerda = number;
+let direita = number ;
+
+
+
+for(let i = 0;i <= number;i+=1){
+    for( let j = 1;j <= n; j+=1){
+        if(j > esquerda && j < direita){
+         linha+="*"
+        } else {
+            linha+=" ";
+        }
+    }
+    console.log(linha)
+    linha="";
+    esquerda-=1;
+    direita+=1;
+}
