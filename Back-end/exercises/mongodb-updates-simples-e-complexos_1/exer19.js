f0dfcf7631fad91f4d4cc198ca6f2de7ab15ad91
Guilherme Almeida <almeidaguilherme204@gmail.com>
2021-03-15 19:09:01 -0300

    exercicios dia 24.2

---
+++ Back-end/exercises/mongodb-updates-simples-e-complexos_1/exer19.js
@@ -0,0 +1,19 @@
+db.xmen.updateMany({
+    $or:
+        [
+            {
+                $and:
+                    [
+                        { occupation: "Senior Staff" },
+                        { power: { $gt: 100 } }
+                    ]
+            },
+            {
+                $and: [
+                    { occupation: "Junior Staff" },
+                    { power: { $gt: 200 } }
+                ]
+            }
+        ]
+},
+    { $currentDate: { lastUpdate: { $type: "timestamp" } }, $set: { area: ["Students Room"] } })
